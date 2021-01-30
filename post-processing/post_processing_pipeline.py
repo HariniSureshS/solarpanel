d9bbfaa7e293785642b4bd0b9a4d5d1882a700d0
Rasha Salim <rasha_gh22@yahoo.com>
2021-01-30 13:13:50 +0300

    Get the georefrenced Polygons

--- post-processing/post_processing_pipeline.py
+++ post-processing/post_processing_pipeline.py
@@ -3,7 +3,6 @@
 
 # In[7]:
 
-
 from solaris.data import data_dir
 import solaris as sol
 import os
@@ -16,11 +15,13 @@ import rasterio as rio
 import pycocotools.mask as mask_util
 import shapely
 import math
-import json
 import numpy as np
+import json
+from shapely.geometry import Polygon
+from osgeo import gdal
 
 class Post_Process:
-    def __init__(self, coco_file , tif_img, crs= 'EPSG:5703'):
+    def __init__(self, coco_file , tif_img):
         '''
         class takes model prediction (coco json format) 
         and reference image, for post processing
@@ -28,10 +29,8 @@ class Post_Process:
         '''
         self.coco_file = coco_file
         self.tif_img = tif_img
-        self.crs = crs
         self.geo_df = None
         self.bin_mask = None
-        
     
     def get_bin_mask(self, threshold):
         '''Take the '''
@@ -62,7 +61,9 @@ class Post_Process:
         '''Full post process mask
         Returns, Tuple (bunary mask and dataFrame)'''
         self.bin_mask = self.get_bin_mask(threshold)
-        self.geo_df = self.__get_geo_df()
+        self.geo_df = self.__get_pix_coords_df()
+        self.geo_df = self.__get_px_coords()
+        self.geo_df = self.__get_geo_coords()
         self.geo_df = self.__get_area()
         self.geo_df = self.__get_azimuth()
         
@@ -71,7 +72,7 @@ class Post_Process:
         
         
     
-    def __get_geo_df(self, output_path=None):
+    def __get_pix_coords_df(self, output_path=None):
         '''
         Takes a tif image and returns a geo dataframe
         '''
@@ -79,20 +80,17 @@ class Post_Process:
             
             if output_path:
                 sol.vector.mask.mask_to_poly_geojson(pred_arr=self.bin_mask, 
-                                                 reference_im=self.tif_img, 
+                                                 reference_im=self.tif_img,
                                                  output_path=output_path,
                                                  output_type='geojson', simplify=True)
             
-                self.geo_df= gpd.read_file(output_path)
-                # Update projection 
-                self.geo_df = self.geo_df.to_crs(self.crs) # in meter
-
+                self.gdf= gpd.read_file(output_path)
             # datagrame was not saved    
             else:
                 geoms = sol.vector.mask.mask_to_poly_geojson(pred_arr=self.bin_mask, 
                                                  reference_im=self.tif_img,  
                                                  output_type='geojson', simplify=True)
-                self.geo_df = gpd.GeoDataFrame(geoms, crs=self.crs)     
+                self.geo_df = gpd.GeoDataFrame(geoms)     
             
             self.geo_df = self.geo_df.drop(['value'], axis = 1) 
             
@@ -148,6 +146,70 @@ class Post_Process:
         
         return self.geo_df
     
+    def __get_px_coords(self):
+        """Function to get geo coordinates given a pixel coordinates"""
+        xs_list = []
+        ys_list = []
+        # Get thelist of pixel lat/long
+       # First we get the pixel lat/long values
+        self.geo_df['pixel_Center_point'] = self.geo_df['geometry'].centroid
+        #Extract lat and lon from the centerpoint
+        self.geo_df["pixel_col"] = self.geo_df.pixel_Center_point.map(lambda p: p.y)
+        self.geo_df["pixel_row"] = self.geo_df.pixel_Center_point.map(lambda p: p.x)
+        
+        return self.geo_df
+    
+    def __get_geo_coords(self):
+        # Now convert the pixel row/col to lat/long
+        # unravel GDAL affine transform parameters
+        coords = [list(poly.exterior.coords) for poly in self.geo_df.geometry]
+        geo_poly = []
+        geo_coords = []
+
+        for poly in coords:
+            geo_poly = []
+            #print(poly)
+            for points in poly:
+                rows = points[0]
+                cols = points[1]
+        
+                (px, py) = rio.transform.xy(self.tif_img.transform, rows, cols, offset='center')
+                poly = (px, py)
+                geo_poly.append(poly)
+        
+            geo_coords.append(geo_poly)
+    
+        geo_polys = []    
+        for poly in geo_coords:
+            geo_polys.append(Polygon(poly))
+        
+        # Add the new polygons to our gdf
+        self.geo_df['polygons'] = geo_polys
+        
+        # Create a dataframe ans set the geometry to the newly created polygons
+        
+        # store the pixel coords
+        pix_polys =  self.geo_df.geometry
+        
+        gdf = gpd.GeoDataFrame(self.geo_df, geometry = self.geo_df.polygons)  
+        gdf['pix_polygons'] = pix_polys
+        
+        # Set the original crs
+        # Reproject to ge the real coords on the map 
+        gdf = gdf.set_crs(str(self.tif_img.crs))
+        # Apply the new projection
+        gdf = gdf.to_crs(epsg=5703)
+        self.geo_df = gdf
+        
+        return self.geo_df
+       
+    
+  
+        
+       
+    
+  
+        
   
         
 
